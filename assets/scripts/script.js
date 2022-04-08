6efdd42ab9b5c2a32b27d9817140f4efc1e1952a
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-04-08 18:00:11 +0200

    Primo MileStone

---
+++ assets/scripts/script.js
@@ -0,0 +1,32 @@
+
+const contenitore = document.querySelector(".container")
+
+const limit = 100
+
+for(let i = 1; i <= limit; i++){
+
+  const boxEr = document.createElement("div");
+
+  boxEr.className = "box";
+  
+
+  if((i % 15)=== 0) {
+
+    boxEr.textContent = "FizzBuzz!";
+  } else if((i % 3)=== 0) {
+
+      boxEr.textContent = "Fizz!";
+
+  }else if((i % 5)=== 0) {
+
+      boxEr.textContent = "Buzz!";
+
+  }else {
+
+    boxEr.append(i);
+
+  }
+
+
+
+}
